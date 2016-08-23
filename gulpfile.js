f2c4fd916ec2bdd5da5aa3e3599a8cd50a43222b
Денис <corvus007@yandex.ru>
2016-08-24 00:33:47 +0300

    Оптимизация графики. Добавлен normalize.css. Доработка пагинации, футера, слайдера. Создание кастомных конторолов в фильтре

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var gulp = require('gulp');
 var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
 var path = require('path');
+var autoprefixer = require('gulp-autoprefixer');
+var concatCss = require('gulp-concat-css');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
 
 gulp.task('svgstore', function() {
   return gulp
@@ -17,6 +22,26 @@ gulp.task('svgstore', function() {
         }]
       }
     }))
-    .pipe(svgstore())
+    .pipe(svgstore({ inlineSvg: true }))
     .pipe(gulp.dest('img/'));
+});
+
+gulp.task('css', function() {
+  return gulp
+    .src('css/style-dev.css')
+    .pipe(plumber())
+    .pipe(concatCss('style.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('css/'))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css/'))
+});
+
+
+gulp.task('watch-css', ['css'], function() {
+  gulp.watch('css/style-dev.css', ['css']);
 });
