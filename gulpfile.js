4979fd023f8861e1f00ca6eab6b45dbebf65621a
Денис <corvus007@yandex.ru>
2016-08-25 02:00:10 +0300

    CSScomb; custom range; filter button; cart product delete; fixed form's elements

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var concatCss = require('gulp-concat-css');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var plumber = require('gulp-plumber');
+var csscomb = require('gulp-csscomb');
 
 
 
@@ -34,6 +35,7 @@ gulp.task('css', function() {
   return gulp
     .src('css/style-dev.css')
     .pipe(plumber())
+    .pipe(csscomb())
     .pipe(concatCss('style.css'))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
