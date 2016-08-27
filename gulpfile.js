c44d971c99dccb75f3fd466a58e3478144b7d9b7
Денис <corvus007@yandex.ru>
2016-08-27 15:48:49 +0300

    Вставлены инлайново SVG-иконки. Доабавлены скрипты. Доработана навигация в футере. Мелкие правки

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,10 @@
 var gulp = require('gulp');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
-var path = require('path');
 var autoprefixer = require('gulp-autoprefixer');
 var concatCss = require('gulp-concat-css');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var plumber = require('gulp-plumber');
 var csscomb = require('gulp-csscomb');
-
-
-
-gulp.task('svgstore', function() {
-  return gulp
-    .src('img/svg-icons/*.svg')
-    .pipe(svgmin(function(file) {
-      var prefix = path.basename(file.relative, path.extname(file.relative));
-      return {
-        plugins: [{
-          cleanupIDs: {
-            prefix: prefix + '-',
-            minify: true
-          }
-        }]
-      }
-    }))
-    .pipe(svgstore({ inlineSvg: true }))
-    .pipe(gulp.dest('img/'));
-});
-
-
 
 gulp.task('css', function() {
   return gulp
@@ -38,7 +13,7 @@ gulp.task('css', function() {
     .pipe(csscomb())
     .pipe(concatCss('style.css'))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      // browsers: ['>1%'],
       cascade: false
     }))
     .pipe(gulp.dest('css/'))
@@ -46,8 +21,6 @@ gulp.task('css', function() {
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('css/'))
 });
-
-
 
 gulp.task('watch-css', ['css'], function() {
   gulp.watch('css/style-dev.css', ['css']);
---
+++ js/script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var btnOpenModal = document.querySelector('.js-open-modal');
+var modal = document.querySelector('.modal');
+var modalOverlay = document.querySelector('.modal-overlay');
+var btnCloseModal = modal.querySelector('.modal__close');
+var feedbackForm = modal.querySelector('.feedback-form');
+var feedbackName = feedbackForm.querySelector('[name=name]');
+var feedbackEmail = feedbackForm.querySelector('[name=email]');
+var feedbackMessage = feedbackForm.querySelector('[name=message]');
+var storageFeedbackName = localStorage.getItem('feedbackName');
+
+
+
+btnOpenModal.addEventListener('click', function(event) {
+  event.preventDefault();
+  modal.classList.add('modal--opened');
+  modalOverlay.classList.add('modal-overlay--opened');
+
+  if (storageFeedbackName) {
+    feedbackName.value = storageFeedbackName;
+    feedbackEmail.focus();
+  } else {
+    feedbackName.focus();
+  }
+
+});
+
+btnCloseModal.addEventListener('click', function(event) {
+  event.preventDefault();
+  modal.classList.remove('modal--opened', 'modal--error');
+  modalOverlay.classList.remove('modal-overlay--opened');
+});
+
+feedbackForm.addEventListener('submit', function(event) {
+  if (!feedbackName.value || !feedbackEmail.value || !feedbackMessage.value) {
+    event.preventDefault();
+    modal.classList.remove('modal--error');
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add('modal--error');
+  } else {
+    localStorage.setItem('feedbackName', feedbackName.value);
+  }
+});
+
+window.addEventListener('keyup', function(event) {
+  console.log(event);
+  if (event.keyCode === 27) {
+    if (modal.classList.contains('modal--opened')) {
+      modal.classList.remove('modal--opened', 'modal--error');
+      modalOverlay.classList.remove('modal-overlay--opened');
+    }
+  }
+});
